3f1c6847bdd6d747a6fc2c4d982fcae9a8324485
[Jeremy Baillargeon] <jbail081@uottawa.ca>
2021-11-16 19:21:04 -0500

    update to product.html and .js file

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,6 @@
 function product_sug() {
     let idea = prompt("Please enter your suggestion");
     if (idea != ""){
-        alert("Thank you for your suggestion!")
+        document.getElementById("product_sug_btn").innerHTML = "Thank you for your suggestion!"
     }
 }
