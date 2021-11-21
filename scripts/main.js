428753d4e907283f25550ad2bf9bf3677a757103
[Jeremy Baillargeon] <jbail081@uottawa.ca>
2021-11-21 15:39:00 -0500

    fixed issue with main.js

--- scripts/main.js
+++ scripts/main.js
@@ -7,7 +7,7 @@ hamburger_button.addEventListener('change', (event) => {
 //For product.html
 function product_sug() {
     let idea = prompt("Please enter your suggestion");
-    if (idea != ""){
+    if (idea != "" && idea != null){
         document.getElementById("product_sug_btn").innerHTML = "Thank you for your suggestion!"
     }
 }
